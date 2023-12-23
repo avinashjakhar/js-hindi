9c17beef05ab3677ec4a1f81a63b447a6a6471ee
avinashjakhar <113355076+avinashjakhar@users.noreply.github.com>
2023-12-23 02:17:47 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+//  let score= "Avinash"
+
+//  console.log(typeof score)
+//  console.log(typeof(score));
+
+//  let valueNumbe = Number(score)
+
+//   console.log(valueNumbe);
+//   console.log(typeof(valueNumbe));
+
+
+// 33 => 33
+// 33ads => NaN
+// true = 1, false = 0
+// String = NaN
+
+let booleanis = 1
+let booleanisLoggin = Boolean(booleanis)
+// console.log(booleanisLoggin);
+// console.log(typeof(booleanisLoggin));
+
+// 1 = true , empty = false , "Avinash" => true
+
+let somenumber = 33
+let StringisNumber = String(somenumber)
+console.log(StringisNumber);
+console.log(typeof(StringisNumber));
