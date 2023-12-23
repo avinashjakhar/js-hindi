d150dd3769642f16473cccff2e5191af1a915217
avinashjakhar <113355076+avinashjakhar@users.noreply.github.com>
2023-12-23 19:34:55 +0000

    change in datatypes

---
+++ 01_basics/05_summaryofdatatypes.js
@@ -0,0 +1,32 @@
+// Primitive
+
+//  Seven types : String,Number,Boolean,Null, UNdefined,Symbol, BigInt
+
+const score= 100
+const scoreValue = 100.4
+const isLooged = false
+
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+// console.log(id == anotherId);
+
+// Non- Primitive
+
+// array, object, Function
+
+const hero =[ "Saktiman", " Nagraj","Doga"];
+let myobj ={
+    name: "Avinash",
+    age : 22,
+
+}
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
