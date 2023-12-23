8a047b06bcf570e7f6f1d0bdfaeeac7330b87085
avinashjakhar <113355076+avinashjakhar@users.noreply.github.com>
2023-12-23 19:05:02 +0000

    comperision of null and undefined values

---
+++ 01_basics/04_comperision.js
@@ -0,0 +1,15 @@
+// console.log("2" == 2);
+// console.log(2 == "2");
+
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+
+// === check the datatypes also
+
+console.log("2" === 2);
