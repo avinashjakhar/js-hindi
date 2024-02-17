78723c73623454a30cd4d04ee39893183a4365c5
avinashjakhar <113355076+avinashjakhar@users.noreply.github.com>
2024-02-17 18:40:07 +0000

    truthy  values

---
+++ 04_control/truthy.js
@@ -0,0 +1,41 @@
+// const userEmail = "Avinash@gmail.com"
+
+// if(userEmail){
+//     console.log("Got user email");
+
+// } else {
+//     console.log("Don't have user email");
+// }
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truth values
+
+// "0",'false', " ",{},[], function(){}
+
+// if (userEmail.length === 0){
+//     console.log("Arraay is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty");
+// }
+
+// Nullish coalescing Operator (??): null undefined
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 10
+// val1 = null ?? 10 >> 23
+// console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ?  console.log("less than 80"): console.log("more than 80");
